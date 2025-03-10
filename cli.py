cc461afc9a8b9499a6e09792cc72b6edb42e1c7c
Yoni Levental <yonilev2021@gmail.com>
2025-03-10 21:28:42 +0200

    Fix and add to to_do_operations.py and cli.py for GUI configuration

--- cli.py
+++ cli.py
@@ -1,7 +1,7 @@
 import todo_operations
 from todo_operations import *
 
-def get_users_choice() -> int:
+'''def get_users_choice() -> int:
     choice = input("""
 Enter 1 to add a task:
 Enter 2 to remove a task:
@@ -15,48 +15,62 @@ Enter 9 to print task's status:
 Enter 0 to exit:\n""")
 
     while choice not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-        choice = input("Invalid. Enter your choice: :")
+        choice = input("Invalid. Enter your choice: :")'''
 
-    return int(choice)
+    #return int(choice)
 
-def main():
-    my_list = todo_operations.ToDoList()
+my_list = todo_operations.ToDoList()
+
+def get_list():
+    return my_list.get_all_tasks()
+
+def main(mission, value_to_add = None, number = None, edit = None):
+    #my_list = todo_operations.ToDoList()
     while True:
-        mission = get_users_choice()
+        #mission = get_users_choice()
         if mission == Options.exit.value:
             break
 
         if mission == Options.add_task.value:
-            my_list.add_task()
+            my_list.add_task(value_to_add)
+            break
 
         elif mission == Options.remove_task.value:
-            my_list.remove_task()
+            my_list.remove_task(number)
+            break
 
         elif mission == Options.edit_task.value:
-            my_list.edit_task()
+            my_list.edit_task(number, edit)
+            break
 
         elif mission == Options.mark_as_done.value:
-            my_list.mark_task("Done")
+            my_list.mark_task("Done", number)
+            break
 
         elif mission == Options.unmark_as_done.value:
-            my_list.mark_task("To Do")
+            my_list.mark_task("To Do", number)
+            break
 
         elif mission == Options.print_all_tasks.value:
-            my_list.print_tasks("To Do", "Done")
+            print("in cli printing all")
+            return my_list.print_tasks("To Do", "Done")
 
         elif mission == Options.print_tasks_to_do.value:
-            my_list.print_tasks("To Do", "H")
+            return my_list.print_tasks("To Do", "H")
 
         elif mission == Options.print_done_tasks.value:
-            my_list.print_tasks("Done", "H")
+            return my_list.print_tasks("Done", "H")
 
         elif mission == Options.print_task_status.value:
-            my_list.print_status()
+            return my_list.print_status(number)
+
 
         elif mission == Options.delete_all_tasks.value:
             my_list.delete_list()
+            break
 
     print("Exit program.")
 
 if __name__ == '__main__':
     main()
+
